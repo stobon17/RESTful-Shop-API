988359afdfc818913e9579aaa3c81021e229139d
stobon17 <simontobon7@gmail.com>
2021-07-10 15:15:20 -0400

    handling CORS

--- app.js
+++ app.js
@@ -4,11 +4,27 @@ const express = require('express');
 const app = express();
 
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 //Sets up middleware, incoming requests go through app.use().
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+//Handle CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    if (req.method === 'OPTIONS')
+    {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 //Routes to handle requests
 app.use('/products', productRoutes);

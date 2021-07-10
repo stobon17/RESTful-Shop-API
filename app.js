2bec394bd09c3f0bc31b32001a6ef01eef682d8f
stobon17 <simontobon7@gmail.com>
2021-07-10 14:53:55 -0400

    error handling and nodemon

--- app.js
+++ app.js
@@ -3,13 +3,29 @@ const express = require('express');
 //Starts express application
 const app = express();
 
+const morgan = require('morgan');
+
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 //Sets up middleware, incoming requests go through app.use().
+app.use(morgan('dev'));
 
-//Anything that reaches /products will use the products routes
+//Routes to handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+app.use((req, res, next) => {
+    const error = new Error('Not Found.');
+    error.status = 404;
+    next(error);
+});
 
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 module.exports = app;
